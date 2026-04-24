6f77b51b3b13e5b41165bdea50d74486db2bf0ec
Walter Moreira <walter@waltermoreira.net>
2026-04-24 00:23:34 -0500

    Fix flake

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         shell = shell-utils.myShell.${system};
-        lean-toolchain = lean-toolchain-nix.packages.${system}.lean-toolchain-4_27;
+        lean-toolchain = lean-toolchain-nix.packages.${system}.lean-toolchain-4_30;
         inherit (lean-toolchain-nix.lib.${system}) buildLeanDeps buildLeanPackageFromDeps;
 
         slidesDeps =
@@ -19,190 +19,57 @@
               "aarch64-darwin" = "";
               "aarch64-linux" = "";
               "x86_64-darwin" = "";
-              "x86_64-linux" = "";
+              "x86_64-linux" = "sha256-am60gxRYbMKUrzhi07s7skkJjORsbJECwebJelSwV3Y=";
             };
           in
           buildLeanDeps {
             name = "slidesDeps";
             src = ./.;
             outputHash = hashes.${system};
-            leanVersion = "4.27.0";
+            leanVersion = "4.30.0-rc2";
             buildPhase = ''
-              lake exe cache get
-              lake build Sequencelib.Meta.OEISTacticHeavy
-              lake build genseq
+              lake build Slides
+              lake build generate-slides
+              lake exe generate-slides
             '';
           };
 
-        genseqBin = buildLeanPackageFromDeps {
-          name = "genseqBin";
+        slides = buildLeanPackageFromDeps {
+          name = "slides";
           src = ./.;
-          leanVersion = "4.27.0";
-          deps = genseqBinDeps;
+          leanVersion = "4.30.0-rc2";
+          deps = slidesDeps;
           buildInputs = [ pkgs.rsync ];
           phases = [ "unpackPhase" "buildPhase" ];
           buildPhase = ''
-            lake build Sequencelib.Meta.OEISTacticHeavy
-            lake build genseq
-            mkdir -p $out/{bin,lib}
-            cp .lake/build/bin/genseq $out/bin
-            rsync -a .lake/build/lib/lean $out/lib/
-            rsync -a .lake/packages/ $out/lib/packages/
-          '';
-        };
-
-        genseq = pkgs.stdenv.mkDerivation {
-          name = "genseq";
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          buildInputs = [ lean-toolchain ];
-          src = ./.;
-          phases = [
-            "buildPhase"
-          ];
-          buildPhase = ''
-            mkdir -p $out/bin
-            LEAN_PATH=$(
-              echo -n "${genseqBin}/lib/lean"
-              for f in $(ls ${genseqBin}/lib/packages/); do
-                echo -n ":${genseqBin}/lib/packages/$f/.lake/build/lib/lean";
-              done
-            )
-            makeWrapper ${genseqBin}/bin/genseq $out/bin/genseq \
-              --set LEAN_PATH "$LEAN_PATH" \
-              --set PATH "$PATH" \
-              --set OEIS_CODOMAINS "${./keywords.json}"
+            lake exe generate-slides 
+            mkdir -p $out
+            rsync -a _slides/ $out/
           '';
         };
         python = pkgs.python313.withPackages (ps: [ ps.supervisor ps.ipython ]);
-
-        sGenseq = pkgs.writeShellApplication {
-          name = "sgenseq";
-          runtimeInputs = with pkgs; [
-            python
-            genseq
-            gnused
-          ];
+        runner = pkgs.writeShellApplication {
+          name = "run-slides";
           text = ''
-            supervisor=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-            port=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-            config=$(mktemp -d)
-            printf "#!${pkgs.stdenv.shell}\n${python}/bin/supervisorctl -c %s \"\$@\"" \
-              "$config/supervisor.conf" > "$config/supervisorctl"
-            chmod +x "$config/supervisorctl"
-            sed -e "s/PORT/$port/" -e "s/SUPERVISOR/$supervisor/" \
-                < ${./supervisord.conf.template} > "$config/supervisor.conf"
-            supervisord -c "$config/supervisor.conf"
-            echo "$port $config/supervisorctl"
-          '';
-        };
-
-        supervisedGenseq = pkgs.writeShellApplication {
-          name = "genseq";
-          runtimeInputs = with pkgs; [
-            python
-            genseq
-            getopt
-            gnused
-            lsof
-            netcat
-          ];
-          text = ''
-            do_kill () {
-              supervisorctl stop all
-              supervisorctl shutdown
-            }
-            do_wait () {
-              while true; do 
-                if nc -z -w1 localhost "$1" >/dev/null 2>&1; then break; fi
-                sleep 1
-              done
-            }
-            args=$(getopt -o "vhp:swnk" -l "version,help,port:,supervise,wait,foreground,kill" -- "$@")
-            eval set -- "$args"
-            supervise=""
-            foreground=""
-            wait=""
-            port="8000"
-            while true
-            do
-            case "$1" in
-            -h|--help)
-              ${genseq}/bin/genseq -h
-              exit 0
-              ;;
-            -v|--version)
-              ${genseq}/bin/genseq --version
-              exit 0
-              ;;
-            -p|--port)
-              shift
-              port="$1"
-              ;;
-            -s|--supervise)
-              supervise="1"
-              ;;
-            -n|--foreground)
-              foreground="-n"
-              ;;
-            -k|--kill)
-              do_kill
-              exit 0
-              ;;
-            -w|--wait)
-              wait="1"
-              ;;
-            --)
-              break
-              ;;
-            esac
-            shift
-            done
-            if [ -z "$supervise" ]; then
-              ${genseq}/bin/genseq -p "$port"
-            else
-              echo "Starting supervised 'genseq' on port $port" | \
-                ${pkgs.boxes}/bin/boxes -d shell -p h2v1
-              if supervisorctl status genseq >/dev/null 2>&1
-              then
-                echo "genseq already running"
-                exit 0
-              fi
-              config=$(mktemp)
-              sed -e "s/PORT/$port/" -e "s/SUPERVISOR/9001/" \
-                < ${./supervisord.conf.template} > "$config"
-              supervisord $foreground -c "$config"
-              if [[ ( -z "$foreground" ) && ( -n "$wait" ) ]]; then
-                echo -n "Waiting for server to be responsive... "
-                do_wait "$port"
-                echo "done"
-              fi
-            fi
+            ${python}/bin/python -m http.server -d ${slides} 
           '';
         };
       in
       {
         packages = {
-          default = supervisedGenseq;
-          genseq = supervisedGenseq;
-          sgenseq = sGenseq;
-          inherit genseqLib genSeqDocs genseqBinDeps genseqBin;
+          default = slides;
+          inherit slides slidesDeps runner;
         };
 
+        apps = {
+          default = {
+            type = "app";
+            program = "${runner}/bin/run-slides";
+          };
+        };
         devShells = {
           default = shell {
-            name = "gen-seq";
-            buildInputs = with pkgs; [
-              lean-toolchain
-              elan
-              go-task
-              python
-              uv
-              findutils
-              lsof
-            ] ++ lib.optional stdenv.isDarwin apple-sdk_14;
-          };
-          plain = shell {
-            name = "gen-seq";
+            name = "tacc-2026";
             buildInputs = with pkgs; [
               lean-toolchain
               elan
